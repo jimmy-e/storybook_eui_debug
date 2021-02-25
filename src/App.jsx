454c8e2b3986b15550e96565ffb3e9e258d91f82
Jimmy Emery <jimmy.e@outlook.com>
2021-02-25 10:38:29 -0500

    updating

--- src/App.jsx
+++ src/App.jsx
@@ -3,22 +3,28 @@ import { EuiFlexGroup, EuiFlexItem, EuiPanel } from '@elastic/eui';
 import Section from './Section';
 
 const App = () => (
-  <EuiFlexGroup>
-    <EuiFlexItem grow={4}>
-      <EuiPanel>
-        <h1>hello world</h1>
-      </EuiPanel>
-    </EuiFlexItem>
-    <EuiFlexItem grow={6}>
-      <EuiFlexGroup direction="column" gutterSize="xl">
-        {Array.from(Array(3).keys()).map((i, index) => (
-          <EuiFlexItem key={index}>
-            <Section />
-          </EuiFlexItem>
-        ))}
-      </EuiFlexGroup>
-    </EuiFlexItem>
-  </EuiFlexGroup>
+  <div
+    style={{
+      padding: '24px',
+    }}
+  >
+    <EuiFlexGroup>
+      <EuiFlexItem grow={4}>
+        <EuiPanel>
+          <h1>hello world</h1>
+        </EuiPanel>
+      </EuiFlexItem>
+      <EuiFlexItem grow={6}>
+        <EuiFlexGroup direction="column" gutterSize="xl">
+          {Array.from(Array(3).keys()).map((i, index) => (
+            <EuiFlexItem key={index}>
+              <Section />
+            </EuiFlexItem>
+          ))}
+        </EuiFlexGroup>
+      </EuiFlexItem>
+    </EuiFlexGroup>
+  </div>
 );
 
 export default App;
